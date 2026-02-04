c74591391ffba1e1b46b906d41ab5cf1c71cde4c
Leo Developer <leo@git.leodev.win>
2026-02-04 21:02:49 +0100

    multiple fixes

--- main.v
+++ main.v
@@ -9,6 +9,7 @@ const vpm_hosts = ['https://vpm.vlang.io', 'https://vpm.url4e.com']
 const mirror_target = 'mirror'
 const is_ci = os.getenv_opt('CI') != none
 const term_width = 60
+const max_concurrent_downloads = 8
 
 fn main() {
 	unbuffer_stdout()
@@ -18,30 +19,49 @@ fn main() {
 
 	os.mkdir_all(mirror_target)!
 
-	mut failed := 0
+	// Channel to receive download results
+	results := chan DownloadResult{cap: packages.len}
+
+	// Semaphore to limit concurrent downloads
+	mut sem := chan int{cap: max_concurrent_downloads}
+	for _ in 0 .. max_concurrent_downloads {
+		sem <- 1
+	}
+
+	// Spawn download tasks
 	for i, package in packages {
-		if is_ci {
-			println('Downloading ${package} (${i}/${packages.len})')
-		} else {
-			print('[${i}/${packages.len}] ${package#[..term_width]}${' '.repeat(term_width - package.len)}\r')
-		}
-		mirror_package(package) or {
+		spawn download_worker(package, i, packages.len, sem, results)
+	}
+
+	// Collect results
+	mut failed := 0
+	for _ in 0 .. packages.len {
+		result := <-results
+		if result.err != '' {
 			failed++
-			eprintln('Error: ${err}')
+			eprintln('Error: ${result.err}')
+		} else if !is_ci {
+			print('[${result.index}/${packages.len}] ${result.package#[..term_width]}${' '.repeat(term_width - result.package.len)}\r')
 		}
 	}
+	println('')
 
 	if failed > 0 {
 		eprintln('Failed to mirror ${failed} packages.')
 	}
 
 	mirrored_packages := os.ls(mirror_target)!
-	for mirrored in mirrored_packages {
-		if mirrored[..mirrored.len - 5] in packages {
-			continue
+
+	// If the upstream freaks out and mass deletes packages, don't delete them in the mirror
+	// 10% seems like a sane limit!
+	if packages.len > mirrored_packages.len * 0.9 {
+		for mirrored in mirrored_packages {
+			if mirrored[..mirrored.len - 5] in packages {
+				continue
+			}
+			println('Removing removed package ${mirrored}')
+			os.rm(mirrored)!
 		}
-		println('Removing removed package ${mirrored}')
-		os.rm(mirrored)!
 	}
 }
 
@@ -55,10 +75,46 @@ fn get_packages() ![]string {
 	return packages.map(it[10..])
 }
 
+struct DownloadResult {
+	package string
+	index   int
+	err     string
+}
+
+fn download_worker(package string, index int, total int, sem chan int, results chan DownloadResult) {
+	// Acquire semaphore slot
+	_ := <-sem
+	defer { sem <- 1 }
+
+	if is_ci {
+		println('Downloading ${package} (${index}/${total})')
+	}
+
+	mirror_package(package) or {
+		results <- DownloadResult{
+			package: package
+			index: index
+			err: '${err}'
+		}
+		return
+	}
+
+	results <- DownloadResult{
+		package: package
+		index: index
+		err: ''
+	}
+}
+
+
 
 fn mirror_package(name string) ! {
 	mirror_file := os.join_path(mirror_target, '${name}.json')
 	upstream := http_get('/api/packages/${name}')!
+
+	if !upstream.body.starts_with('{') {
+		return error('Package not found')
+	}
 
 	parsed := json2.decode[json2.Any](upstream.body)!
 	mut top := parsed.as_map()
